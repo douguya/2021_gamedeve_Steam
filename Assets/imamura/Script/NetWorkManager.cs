2c74c92361e388e5fa7d9208a8b18f64fcf48960
DESKTOP-4T4O2VV\nekut <imamurasyunnsuke.patagpnia@gmail.com>
2022-02-12 07:07:33 +0900

    no message

--- Assets/imamura/Script/NetWorkManager.cs
+++ Assets/imamura/Script/NetWorkManager.cs
@@ -75,7 +75,7 @@ public class NetWorkManager : MonoBehaviourPunCallbacks
 
 
         // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
-        var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        var position = new Vector3(-7.69f, -3.66f);
         PhotonNetwork.Instantiate("Avatar", position, Quaternion.identity);
 
 
