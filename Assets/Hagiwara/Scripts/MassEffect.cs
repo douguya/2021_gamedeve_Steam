1c6f6506d0967e5707287af21ddd6514d8197385
Mika-2143 <kinmdai0403@gmail.com>
2022-02-24 17:56:45 +0900

    ホップアップ

--- Assets/Hagiwara/Scripts/MassEffect.cs
+++ Assets/Hagiwara/Scripts/MassEffect.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class MassEffect : MonoBehaviour
 {
     //public GameObject[] Player = new GameObject[4];//プレイヤーオブジェクト取得
+    public GameObject daycomment;
+
+    public float speed = 0.5f;                      //プレイヤー移動速度
+    private float currentTime = 0f;
+    private int Count = 0;
     void Start()
     {
         
@@ -19,44 +24,35 @@ public class MassEffect : MonoBehaviour
     public void Effects(string day)
     {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        GetComponent<PlayerStatus>().daycomment(day);
+        
         Debug.Log(DictionaryManager.DayEffectictDictionary[day][0]);
        // Debug.Log("RRRRRRRRRRRRR" + DictionaryManager.DayEffectictDictionary[day][0,0]);
         switch (DictionaryManager.EffectictCategoryDictionary[DictionaryManager.DayEffectictDictionary[day][0]][0])
         {
             case "アイテム":
                 GetItem( DictionaryManager.EffectictCategoryDictionary[DictionaryManager.DayEffectictDictionary[day][0]][1]);
-                step();
                 break;
 
             case "アイコン":
-                step();
+
                 break;
 
             case "BGM":
-                step();
                 break;
 
 
             case "背景":
-                step();
+                break;
+
+            case "波":
+                
                 break;
 
             case "":
-                step();
+                break;
+
+            default:
                 break;
         }
         
