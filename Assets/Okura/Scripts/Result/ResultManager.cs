95130684bc9b6191bda15377f0991d7724f3c0dd
Okurabacon <s2023166@gmail.com>
2022-02-25 00:48:18 +0900

    画像素材の入れ替えと1位に王冠が表示されるようになりました

--- Assets/Okura/Scripts/Result/ResultManager.cs
+++ Assets/Okura/Scripts/Result/ResultManager.cs
@@ -17,6 +17,8 @@ public class ResultManager : MonoBehaviour
     [SerializeField]
     Transform[] ScoreBackGround;      //テキストボックスの親にするオブジェクト(以下SBG)
     [SerializeField]
+    GameObject[] PlayerBackGround;
+    [SerializeField]
     Button Itemadd;
     [SerializeField]
     Button display;
@@ -46,7 +48,7 @@ public class ResultManager : MonoBehaviour
 
 
         //ここからプレハブを生成するための下準備
-        GameObject[] PlayerBackGround = new GameObject[playersnum];
+        PlayerBackGround = new GameObject[playersnum];
         float[] PBGinitpos = new float[2] {-235.0f, -16.9f};//複製する際の初期位置xyご自由に変えてください
         float PBGinterval = 160.0f;//複製する際のx座標の間隔*/
 
@@ -77,12 +79,12 @@ public class ResultManager : MonoBehaviour
     {
         textUI = GameObject.Find("Items").GetComponent<Text>();
         float[] initpos = new float[2] { textUI.transform.localPosition.x, textUI.transform.localPosition.y };//テキストの初期位置
+        int[] totalpoint = new int[playersnum];
         int count = 0;
         DuplicateItem();        //テストのためここに入れました。実際のゲームシーンではアイテムを追加する必要がないため、Awakeにいれてください
 
         foreach(Transform i in ScoreBackGround)
         {
-            int totalpoint = 0;
             int dupcount = 0;
             foreach (string j in Items[count].Keys)
             {
@@ -97,33 +99,51 @@ public class ResultManager : MonoBehaviour
                 point.alignment = TextAnchor.MiddleRight;
                 point.text = Items[count][j] + "P";
 
-                totalpoint += Items[count][j];
+                totalpoint[count] += Items[count][j];
                 dupcount++;
             }
 
-            total[count].text = totalpoint.ToString() + "P";
+            total[count].text = totalpoint[count].ToString() + "P";
             count++;
         }
+
+        int[] Rank = new int[4] { 0, 1, 2, 3 };
+        for (int i = 0; i < playersnum; ++i)
+        {
+            for (int j = i + 1; j < playersnum; ++j)
+            {
+                if(totalpoint[i] < totalpoint[j])
+                {
+                    Debug.Log(totalpoint[i] + "は" + totalpoint[j] +"より小さい");
+                    int tmp;
+                    tmp = Rank[i];
+                    Rank[i] = Rank[j];
+                    Rank[j] = tmp;
+                }
+                else
+                {
+                    Debug.Log(totalpoint[i] + "は" + totalpoint[j] + "より大きい");
+                }
+            }
+        }
+        Image RImage = GameObject.Find("Rank" + Rank[0]).GetComponent<Image>();
+        RImage.sprite = Resources.Load<Sprite>("1st");
     }
+
 
     //重複アイテムをまとめてすっきりさせる
     void DuplicateItem()
     {
         for (int num = 0; num < OriginalItem.Length; num++)//プレイヤーの数と同じ回数行う
         {
-            Debug.Log(num + "人目");
             for (int i = 0; i < OriginalItem[num].Count; i++)//num番目のプレイヤーの持ち物の数だけ行う
             {
-                Debug.Log(i + "回目");
-                Debug.Log(OriginalItem[num][i]);
                 if (Items[num].ContainsKey(OriginalItem[num][i]))//アイテムの中に重複する者があれば
                 {
-                    Debug.Log("A");
                     Items[num][OriginalItem[num][i]] += DictionaryManager.ItemDictionary[OriginalItem[num][i]][0];//ポイントを増やす
                 }
                 else
                 {
-                    Debug.Log("B");
                     Items[num].Add(OriginalItem[num][i], DictionaryManager.ItemDictionary[OriginalItem[num][i]][0]);//なければ新しい項目を作る
                 }
             }
