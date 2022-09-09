1040f8e634e4f55f852dadc2981fdc597b427c88
Mika-2143 <kinmdai0403@gmail.com>
2022-09-10 05:39:29 +0900

    Item画像、ダイス2D化、日付効果Log追加

--- Assets/taki/newRotate.cs
+++ Assets/taki/newRotate.cs
@@ -1,53 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class newRotate : MonoBehaviour
 {
-    public float xSpeed, ySpeed, zSpeed;　//各軸の回転速度
-    public bool rotate; //回転を止めるボタン
+    //public float xSpeed, ySpeed, zSpeed;　//各軸の回転速度
+    //public bool rotate; //回転を止めるボタン
 
     public int DiceNum; //出たさいころの目
 
-    private float xKeep, yKeep, zKeep; //回転速度の保存用
-    private float xShow, zShow; //さいころの目を見せるときの角度
+    //private float xKeep, yKeep, zKeep; //回転速度の保存用
+    //private float xShow, zShow; //さいころの目を見せるときの角度
 
     //廃止　public GameObject[] Dice = new GameObject[6];　//ダイスの各面に張り付けてある空箱
     //廃止　public GameObject max; //一番上の面の空箱
+
+    public Sprite[] Dise_sprite = new Sprite[6];
 
     public List<int> InDiceNum = new List<int> { 1, 2, 3, 4, 5, 6 }; //指定された数がさいころから出る
 
     // Start is called before the first frame update
     void Start()
     {
-        xKeep = xSpeed;
-        yKeep = ySpeed;
-        zKeep = zSpeed;
-        newDiceStop();
+        //xKeep = xSpeed;
+        //yKeep = ySpeed;
+        //zKeep = zSpeed;
+        //newDiceStop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime));
-        //回転させてる
+        //transform.Rotate(new Vector3(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime));
+        ////回転させてる
 
-        //trueの場合常に回転する
-        if (rotate == true)
-        {
-            xSpeed = xKeep;
-            ySpeed = yKeep;
-            zSpeed = zKeep;
-        }
+        ////trueの場合常に回転する
+        //if (rotate == true)
+        //{
+        //    xSpeed = xKeep;
+        //    ySpeed = yKeep;
+        //    zSpeed = zKeep;
+        //}
 
 
     }
 
     //回すときの呼び出し用関数
-    public void RotateStart()
-    {
-        rotate = true;
-    }
+    //public void RotateStart()
+    //{
+    //    rotate = true;
+    //}
 
     //ストップするときの呼び出し用関数
     public void newDiceStop()
@@ -61,37 +64,43 @@ public class newRotate : MonoBehaviour
                 break;//数字が許可されていれば通す
             }
         }
-
+        Output_LookDice();
         //さいころを停止
-        rotate = false;
-        xSpeed = 0;
-        ySpeed = 0;
-        zSpeed = 0;
+        //rotate = false;
+        //xSpeed = 0;
+        //ySpeed = 0;
+        //zSpeed = 0;
 
         //出た目から方向をセット
-        switch (DiceNum)
-        {
-            case 1:
-                xShow = 0; zShow = 0;
-                break;
-            case 2:
-                xShow = 0; zShow = 90;
-                break;
-            case 3:
-                xShow = -90; zShow = 0;
-                break;
-            case 4:
-                xShow = 90; zShow = 0;
-                break;
-            case 5:
-                xShow = 0; zShow = -90;
-                break;
-            case 6:
-                xShow = 180; zShow = 0;
-                break;
-        }
+        //switch (DiceNum)
+        //{
+        //    case 1:
+
+        //        xShow = 0; zShow = 0;
+        //        break;
+        //    case 2:
+        //        xShow = 0; zShow = 90;
+        //        break;
+        //    case 3:
+        //        xShow = -90; zShow = 0;
+        //        break;
+        //    case 4:
+        //        xShow = 90; zShow = 0;
+        //        break;
+        //    case 5:
+        //        xShow = 0; zShow = -90;
+        //        break;
+        //    case 6:
+        //        xShow = 180; zShow = 0;
+        //        break;
+        //}
         //さいころの目を見せる
-        transform.rotation = Quaternion.Euler(xShow, 0, zShow);
+        //transform.rotation = Quaternion.Euler(xShow, 0, zShow);
+    }
+
+    private void Output_LookDice()
+    {
+        gameObject.GetComponent<Image>().sprite = Dise_sprite[DiceNum - 1];
     }
 
     public void OddDice() //奇数ダイスになる
